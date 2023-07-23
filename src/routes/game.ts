deadd78fc380fc9ae6c7b2d327fa11d286ce00fc
Anuraag Barde <anuraabarde@gmail.com>
2023-07-23 23:03:12 +0530

    added /game/create route

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -4,15 +4,18 @@ const router = express.Router();
 import * as GameService from '@services/gameService';
 
 router.post('/create', (req, res) => {
-  res.status(200);
-  const input: {
-    userIds: Array<string>;
-  } = req.body;
-
-  res.json({
-    // id: req.params.id,
-  });
-  return res;
+  try {
+    const input: {
+      userIds: Array<string>;
+    } = req.body;
+    const response = GameService.create(input);
+    res.status(200).json(response);
+    return res;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+    return res;
+  }
 });
 
 export default router;
