97d32449fb5ed27bbf38cc16208f6fc80f091499
barde <barde@bardes-MacBook-Air.local>
2023-07-23 13:01:44 +0530

    added additional game logic

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -5,7 +5,7 @@ import { Coordinate, Move } from '@models/Board/Move';
 
 class Game {
   id: string;
-  nextPlayerMove: PlayingUser | null; //or just PlayingUser.Id?
+  nextPlayerMoveIndex: number; //or just PlayingUser.Id?
   outcome: Outcome;
   board: Board;
   playingUsers: Array<PlayingUser>;
@@ -14,22 +14,40 @@ class Game {
   constructor(players: Array<PlayingUser>, BoardSize: number) {
     this.playingUsers = players;
     this.id = '55'; //should generate from db
-    this.nextPlayerMove = this.getRandomPlayer(this.playingUsers); // I think it is better to pass in explicty rather than guess what side effects this.getRandomPlayer will have
+    this.nextPlayerMoveIndex = this.getRandomPlayerIndex(this.playingUsers); // I think it is better to pass in explicty rather than guess what side effects this.getRandomPlayer will have
     this.outcome = OUTCOME.READY;
     this.moveHistory = [];
     this.board = new Board(BoardSize);
   }
 
-  getRandomPlayer(playingUsers: Array<PlayingUser>): PlayingUser {
+  getRandomPlayerIndex(playingUsers: Array<PlayingUser>): number {
     //using this pointer here feels wrong, since we are using global state, inside the class,
     // I think better would be this.getRandomPlayer(this.playingUsers);
     // This will make it pure
-    return playingUsers[Math.floor(Math.random() * playingUsers.length)];
+    return Math.floor(Math.random() * playingUsers.length);
   }
 
   playMove(player: PlayingUser, x: number, y: number) {
     const move = new Move(player, x, y);
+    this.validateMove(move);
     this.moveHistory.push(move); //side effect
     this.board.updateBoard(move); //side effect
+    //check and update outcome
+  }
+
+  validateMove(move: Move) {
+    if (move.location.x < 0 || move.location.y >= this.board.size) {
+      throw new Error(`Invalid x coordinate)`);
+    }
+    if (move.location.y < 0 || move.location.y >= this.board.size) {
+      throw new Error(`Invalid y coordinate)`);
+    }
+    if (this.playingUsers[this.nextPlayerMoveIndex]?.id !== move.user.id) {
+      throw new Error(`Player ${move.user.name} is not allowed to move currently`);
+    }
+  }
+
+  getBoard() {
+    return this.board.state;
   }
 }
